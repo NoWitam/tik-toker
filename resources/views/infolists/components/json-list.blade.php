53412527bb4ae30720566905cb4ddc368c1fdf79
NoWitam <wowek31@gmail.com>
2023-12-22 21:47:05 +0100

    TT-2

--- resources/views/infolists/components/json-list.blade.php
+++ resources/views/infolists/components/json-list.blade.php
@@ -1,4 +1,9 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
+    <style>
+        .json-string {
+            white-space: pre-line;
+        }
+        </style>
     <div style="width: 850px" x-data='{
         get prettyJson() {
             return window.prettyPrint({!! $getState() !!})
