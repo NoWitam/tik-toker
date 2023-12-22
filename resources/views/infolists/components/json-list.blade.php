1908bbb6572248dac4bbc1dacbccf5ac80ccbd2b
NoWitam <wowek31@gmail.com>
2023-12-22 20:31:35 +0100

    TT-2

--- resources/views/infolists/components/json-list.blade.php
+++ resources/views/infolists/components/json-list.blade.php
@@ -1,12 +1,12 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
-    <div x-data="{
+    <div x-data='{
         get prettyJson() {
-            json = '{{$getState()}}';
-            json = json.replace(/&quot;/g, '\\"').replace(/\\n/g, '').replace(/\n/g, '');
+            json = "{{$getState()}}";
+            json = json.replace(/&quot;/g, "\\"").replace(/\\n/g, "").replace(/\n/g, "");
             json = JSON.parse(json);
             return window.prettyPrint(json);
         }
-    }">
+    }'>
         <pre class="prettyjson" x-html="prettyJson"></span>
     </div>
 </x-dynamic-component>
