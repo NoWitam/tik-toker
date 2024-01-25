a97d7770715b2938cb4138b319325fd9edbdded0
NoWitam <wowek31@gmail.com>
2024-01-25 05:07:14 +0100

    TT-12

---
+++ app/Jobs/UploadVideo.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Exceptions\ReleaseActionableJobException;
+use App\Models\Content;
+use App\Models\Enums\ContentStatus;
+use App\Models\Interfaces\Actionable;
+use HeadlessChromium\BrowserFactory;
+use HeadlessChromium\Communication\Message;
+use Illuminate\Support\Carbon;
+
+class UploadVideo extends ActionableJob
+{
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        public Content $content,
+    ){}
+
+    public function getActionable() : Actionable
+    {
+        return $this->content;
+    }
+
+    public function getType() : string
+    {
+        return 'Opublikuj wideo';
+    }
+
+    public function run() : array
+    {
+        $this->content->refresh();
+
+        if($this->content->status != ContentStatus::CREATED) {
+            throw new ReleaseActionableJobException(Carbon::now()->addMinute());
+        }
+
+        $browserFactory = new BrowserFactory(config('app.chromium'));
+        $browser = $browserFactory->createBrowser([
+          'headless' => true, // disable headless mode
+          'sendSyncDefaultTimeout' => 20000,
+          'connectionDelay' => 500
+        ]);
+
+        try {
+            $page = $browser->createPage();
+
+            $page->navigate('https://www.tiktok.com/')->waitForNavigation();
+            $page->evaluate('document.cookie = "sessionid=' . $this->content->series->account->access_token . '; expires=Fri, 31 Dec 9999 23:59:59 GMT";');
+            $page->navigate('https://www.tiktok.com/creator#/upload?scene=creator_center')->waitForNavigation();
+
+            $page->waitUntilContainsElement('input');
+            $input = $page->dom()->querySelector('input');
+            $input->sendFile($this->getFolderName() . "/video.mp4");
+
+            sleep(10);
+
+            $x = $page->callFunction("
+                    function() {\n return document.querySelector('.DraftEditor-root').getBoundingClientRect().x;\n}"
+                )->getReturnValue();
+            $y = $page->callFunction("
+                function() {\n return document.querySelector('.DraftEditor-root').getBoundingClientRect().y;\n}"
+            )->getReturnValue();
+            $page->mouse()->move($x, $y)->click();
+
+            $keyboard = $page->keyboard()->press('Control')->type('a')->type('x')->release('Control');
+            $keyboard->setKeyInterval(10)->typeText($this->content->script['title']);
+
+            $keyboard->press("Shift");
+            $page->getSession()->sendMessageSync(new Message('Input.dispatchKeyEvent', [
+                'type' => 'rawKeyDown',
+                'windowsVirtualKeyCode' => 13,
+            ]));
+            $keyboard->release("Shift");
+
+            foreach($this->content->script['hashtags'] as $hashtag)
+            {
+                $page->evaluate("document.querySelector('.icon-style.hash').click();");
+                $hashtag = mb_str_split(str_replace("#", "", $hashtag));
+                foreach($hashtag as $key)
+                {
+                    $keyboard->type($key);
+                }
+                sleep(1);
+                $page->getSession()->sendMessageSync(new Message('Input.dispatchKeyEvent', [
+                    'type' => 'rawKeyDown',
+                    'windowsVirtualKeyCode' => 13,
+                ]));
+            }
+
+            $page->evaluate('document.querySelector(\'.btn-post button\').click()');
+
+            $this->content->update([
+                'publicated_at' => Carbon::now()
+            ]);
+
+        } finally {
+            $browser->close();
+        }
+
+        return [
+            'info' => "Opublikowano o " . Carbon::now()->format('Y-m-d H:i:s')
+        ];
+    }
+
+    private function getFolderName()
+    {
+        $className = explode("\\", Content::class);
+        $className = array_pop($className);
+        return "Models/{$className}/{$this->content->id}";
+    }
+}
--- app/Models/Traits/HasActions.php
+++ app/Models/Traits/HasActions.php
@@ -3,12 +3,33 @@
 namespace App\Models\Traits;
 
 use App\Models\Action;
+use App\Models\Enums\ActionStatus;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 trait HasActions
 {
     public function actions() : MorphMany
     {
         return $this->morphMany(Action::class, 'actionable');
+    }
+
+    public static function bootHasActions()
+    {
+        static::deleting(function ($record) {
+
+            $now = Carbon::now()->format("Y-m-d H:i:s");
+
+            foreach($record->actions()->where('status', ActionStatus::WAITING)->get() as $action)
+            {
+                DB::table('jobs')->where('payload', 'like', '%"uuid":"' . $action->job_uuid . '"%')->delete();
+
+                $action->update([
+                    'status' => ActionStatus::FAILED,
+                    'info' => "Zasób usunięto o {$now}"
+                ]);
+            }
+        });
     }
 }
